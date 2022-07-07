f87c5260294525c0e74441bebc4a1a6136d8fc54
Irfankhan761 <97773783+Irfankhan761@users.noreply.github.com>
2022-07-07 13:11:12 +0500

    update code file
    update code

---
+++ Code/src/Data_access_layer/TeacherRegistration.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//mine
+package Data_access_layer;
+
+import java.sql.*;
+
+/**
+ *
+ * @author mnk
+ */
+public class TeacherRegistration {
+       void regesterteacher(String fname,String lname, String t_id, String email,String Class,String pass,String admin_id,String query,PreparedStatement prepere) throws SQLException{
+   prepere.setString(1, fname);
+   prepere.setString(2, lname);
+   prepere.setString(3, t_id);
+   prepere.setString(4, email);
+   prepere.setString(5, Class);
+   prepere.setString(6, pass);
+   prepere.setString(7,admin_id );
+   
+ 
+ prepere.executeUpdate();
+
+       }
+}
---
+++ Code/src/Record/StudentAttendanceRecord.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Record;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author hp
+ */
+public class StudentAttendanceRecord {
+    public String regno;
+    public String attendance;
+    public String teacherid;
+    public String date;
+    
+    
+}
